26084cac0f3a84155fe4b8963a875c5b323cc5b7
WajiHaiderAkhtar <natikhaider157@gmail.com>
2025-07-04 21:32:58 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(cors({origin: 'https://freeimagetopdf.onrender.com'}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
